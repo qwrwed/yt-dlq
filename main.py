c982b944b01ca09662700e3bec0131155dabfa03
qwrwed <qwrwed@users.noreply.github.com>
2022-06-21 03:08:28 +0100

    simple GUI with channel list

---
+++ main.py
@@ -0,0 +1,53 @@
+import os
+
+import PySimpleGUI as sg
+
+from utils import download
+
+INPUT_FILE = os.path.join("data", "to_download.txt")
+DOWNLOAD_ARCHIVE = os.path.join("data, download_archive.txt")
+WINDOW_TITLE = "Window Title"
+
+
+def download_binary():
+    binary_url = "https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp.exe"
+    download(binary_url, binary_url.split("/")[-1])
+
+
+with open(INPUT_FILE) as f:
+    multiline_text_prev = f.read()
+
+# sg.theme('DarkGrey9')
+
+layout = [
+    [
+        sg.Multiline(
+            key="-ML-",
+            size=(60, 20),
+            default_text=multiline_text_prev,
+            enable_events=True,
+        )
+    ],
+    [sg.Button("Save")],
+]
+window = sg.Window(WINDOW_TITLE, layout)
+
+while True:
+    event, values = window.read()
+
+    if event == sg.WIN_CLOSED:
+        break
+
+    multiline_text_curr = values["-ML-"]
+    if event == "Save":
+        with open(INPUT_FILE, "w") as f:
+            f.write(multiline_text_curr)
+        multiline_text_prev = multiline_text_curr
+
+    if multiline_text_curr != multiline_text_prev:
+        window.set_title(f"{WINDOW_TITLE}*")
+    else:
+        window.set_title(f"{WINDOW_TITLE}")
+
+
+window.close()
---
+++ utils.py
@@ -0,0 +1,26 @@
+def download(url, filename):
+    # https://stackoverflow.com/a/63831344
+    import functools
+    import pathlib
+    import shutil
+    import requests
+    from tqdm.auto import tqdm
+
+    r = requests.get(url, stream=True, allow_redirects=True)
+    if r.status_code != 200:
+        r.raise_for_status()  # Will only raise for 4xx codes, so...
+        raise RuntimeError(f"Request to {url} returned status code {r.status_code}")
+    file_size = int(r.headers.get("Content-Length", 0))
+
+    path = pathlib.Path(filename).expanduser().resolve()
+    path.parent.mkdir(parents=True, exist_ok=True)
+
+    desc = "(Unknown total file size)" if file_size == 0 else ""
+    r.raw.read = functools.partial(
+        r.raw.read, decode_content=True
+    )  # Decompress if needed
+    with tqdm.wrapattr(r.raw, "read", total=file_size, desc=desc) as r_raw:
+        with path.open("wb") as f:
+            shutil.copyfileobj(r_raw, f)
+
+    return path
